29881f89aa6838a7762a9c20f3080a6444780a9f
Michael Merene <jmerene83@academic.rrc.ca>
2021-10-27 23:34:13 -0500

    add home page and change route to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'champions/index'
-  get 'champions/show'
-  get 'home/index'
+  root 'home#index'
+  resources :champions, only: %i[index show]
+  resources :partypes, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
