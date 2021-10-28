f753e98ad85dbd2080bf7379c73ab3cb895b3015
Michael Merene <jmerene83@academic.rrc.ca>
2021-10-28 12:37:00 -0500

    modified the champion index

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -1,7 +1,9 @@
 class ChampionsController < ApplicationController
   def index
+    @champions = Champion.includes(:partype).all.order("name ASC")
   end
 
   def show
+    @champion = Champion.find(params[:id])
   end
 end
