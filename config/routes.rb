780a7c1dcecd8727f20a2d6597eff55984290035
Michael Merene <jmerene83@academic.rrc.ca>
2021-10-28 20:50:52 -0500

    added search bar but not functional yet

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :champions, only: %i[index show]
   resources :partypes, only: %i[index show]
+  resources :champions, only: %i[index show] do
+    collection do 
+      get "search"
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
