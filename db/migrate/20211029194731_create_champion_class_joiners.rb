8208d7ec7a234f26cf4240d85e4a0f46dc7ca6fd
Michael Merene <jmerene83@academic.rrc.ca>
2021-10-29 14:48:04 -0500

    added champion Class joiner table

---
+++ db/migrate/20211029194731_create_champion_class_joiners.rb
@@ -0,0 +1,10 @@
+class CreateChampionClassJoiners < ActiveRecord::Migration[6.1]
+  def change
+    create_table :champion_class_joiners do |t|
+      t.references :champion, null: false, foreign_key: true
+      t.references :championclass, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
